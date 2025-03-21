b9037ebfe7f9db5c830e772e8ef48d7ea08a1a4b
saniiyya <saniyaanoopgavankar@gmail.com>
2025-03-21 11:44:44 +0530

    saniya Update List_code.py

--- List_code.py
+++ List_code.py
@@ -1,3 +1,18 @@
+from machine import Pin, PWM
+enA = PWM(Pin(15))
+in1 = Pin(12,Pin.OUT)
+in2 = Pin (13,Pin.OUT)
+
+enA.freq(1000)
+enA.duty(512)
+
+while True :
+    in1.value(1)
+    in2.value(0)
+    time.sleep(2)
+    in1.value (0)
+    in2.value(1)
+    time.sleep(2)
 # Write your code here :-)
 #Practise of repo for section B
 
